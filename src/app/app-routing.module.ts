60fd8a66e89bf8ac6b6e3280040441b135e72d81
tris <kaynguywn22898@gmail.com>
2024-05-08 10:52:36 +0700

    abc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
       import('./pages/pages.module').then((m) => m.PagesModule),
   },
 
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
 ];
 @NgModule({
